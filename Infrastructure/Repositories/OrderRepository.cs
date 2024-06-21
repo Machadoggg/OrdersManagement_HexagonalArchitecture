a50215dfcb0a239107af47d04eb39d07e61aecfe
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-20 23:08:10 -0500

    OrderRepository implementation defined in core

---
+++ Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Infrastructure.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly List<Order> _orders = new List<Order>();
+
+        public Order GetOrderById(int id)
+        {
+            return _orders.FirstOrDefault(o => o.Id == id);
+        }
+
+        public void AddOrder(Order order)
+        {
+            _orders.Add(order);
+        }
+    }
+}
