165efeab8f9fe44b3001fc02ae4f50a183b8d2bc
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-20 23:47:02 -0500

    Add Orders Controller, add list objects

---
+++ API/Controllers/OrderController.cs
@@ -0,0 +1,39 @@
+﻿using Application.Services;
+using Core.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly OrderService _orderService;
+
+        public OrderController(OrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetOrder(int id) 
+        {
+            var order = _orderService.GetOrder(id);
+
+            return Ok(order);
+        }
+
+        [HttpPost]
+        public IActionResult CreateOrder(Order order)
+        {
+            _orderService.CreateOrder(order);
+
+
+            // Build URI
+            var uri = Url.Action("GetOrder", new { id = order.Id });
+            return Created(uri, order);
+;
+        }
+    }
+}
--- Infrastructure/Repositories/OrderRepository.cs
+++ Infrastructure/Repositories/OrderRepository.cs
@@ -8,7 +8,12 @@ namespace Infrastructure.Repositories
 {
     public class OrderRepository : IOrderRepository
     {
-        private readonly List<Order> _orders = new List<Order>();
+        private readonly List<Order> _orders = new List<Order>()
+             {
+            new Order { Id = 1, ProductName = "Product A", Quantity = 2, Price = 10.99m },
+            new Order { Id = 2, ProductName = "Product B", Quantity = 1, Price = 5.99m },
+            new Order { Id = 3, ProductName = "Product C", Quantity = 5, Price = 20.00m }
+        };
 
         public Order GetOrderById(int id)
         {
